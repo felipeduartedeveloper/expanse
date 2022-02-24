d25099b60acd914feac62324d8c4a45e53fc4f0c
felipeduartedelevoper <felipesilvapac@gmail.com>
2022-02-24 17:41:13 -0300

    add style package

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from 'styled-components'
+import '../styles/globals.css';
+import { GlobalStyle } from '../styles/globalStyle';
+import { theme } from '../styles/theme';
+
+
+
+
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+      <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
